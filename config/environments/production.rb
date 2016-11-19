b13c661b865d6419eeb41a4fdd0abfb0b32ed7fd
Pascal Pencil <pascal@superpencil.com>
2016-11-19 17:35:09 +0900

    Fifth attempted fix for S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,8 @@ Rails.application.configure do
   # Get S3 config from Heroku vars
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_region => ENV.fetch('AWS_REGION'),
+    :s3_host_name => "s3-#{ENV.fetch('AWS_REGION')}.amazonaws.com",
     :s3_credentials => {
       :bucket => ENV.fetch('S3_BUCKET_NAME'),
       :access_key_id => ENV.fetch('AWS_ACCESS_KEY_ID'),
