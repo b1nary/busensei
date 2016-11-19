11fcda1f19cd2aafec8594ea25d29aac8816d506
Pascal Pencil <pascal@superpencil.com>
2016-11-19 17:58:52 +0900

    ugh

--- db/migrate/20161119084555_change_comics_to_illustrations.rb
+++ db/migrate/20161119084555_change_comics_to_illustrations.rb
@@ -1,5 +1,5 @@
-class ChangeIllustrationsToIllustrations < ActiveRecord::Migration[5.0]
+class ChangeComicsToIllustrations < ActiveRecord::Migration[5.0]
   def change
-    rename_table :illustrations, :illustrations
+    rename_table :comics, :illustrations
   end
 end
