d4a80c876b72ae2b95299b4fa70b8f1de3fabcc4
BuSensei <busensei@gmail.com>
2016-11-23 19:29:26 +0900

    ABC ascending

--- app/controllers/kanjis_controller.rb
+++ app/controllers/kanjis_controller.rb
@@ -4,7 +4,7 @@ class KanjisController < ApplicationController
   # GET /kanjis
   # GET /kanjis.json
   def index
-    @kanjis = Kanji.all.order(reading: :desc).page(params[:page]).per(50)
+    @kanjis = Kanji.all.order(reading: :asc).page(params[:page]).per(50)
     # @kanjis = Kanji.search { fulltext 'Vocab' }
   end
 
