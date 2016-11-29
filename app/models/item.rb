5e25a3655f9bdeb615ee6f3932f11ece9e65c3a3
Roman Pramberger <b1nary@users.noreply.github.com>
2016-11-29 09:13:41 +0000

    :)

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,15 @@ class Item < ApplicationRecord
     attributes :category
   end
 
+  def to_param
+    self.slug
+  end
+  
+  before_save :create_slug
+  
+  private
+ 
   def create_slug
     self.slug = self.word.downcase.strip.gsub(' ', '-')
   end
-
-  before_save :create_slug
 end
